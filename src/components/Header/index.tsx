e798ddbf25a31bb2ad6e90a0fc3ed7741cfa6e7a
Masud Rana <masud202350@gmail.com>
2025-08-10 00:31:13 +0600

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,118 +15,118 @@ import { useTranslation } from 'react-i18next'
 import { useRouter } from 'next/router'
 
 export function Header() {
-  const [open, setOpen] = useState(false);
-  const { currentTheme, toggleTheme } = useThemeContext(); 
-  const handleOpen = () => {
-    setOpen(!open);
-  };
-  const { t, i18n } = useTranslation('common');
-  const router = useRouter();
-  const [currentLang, setCurrentLang] = useState<'en' | 'ta'>('en');
-  useEffect(() => {
-    const { locale } = router;
-    setCurrentLang(locale as 'en' | 'ta');
-  }, [router.locale]);
+  const [open, setOpen] = useState(false);
+  const { currentTheme, toggleTheme } = useThemeContext(); 
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+  const { t, i18n } = useTranslation('common');
+  const router = useRouter();
+  const [currentLang, setCurrentLang] = useState<'en' | 'bn'>('en'); // Changed 'ta' to 'bn'
+  useEffect(() => {
+    const { locale } = router;
+    setCurrentLang(locale as 'en' | 'bn'); // Changed 'ta' to 'bn'
+  }, [router.locale]);
 
-  return (
-    <HeaderContainer style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1000 }}>
-      <div className="mobile-content" style={{ display: 'flex' }}>
-        <Link href={'/'}>
-          <div className="logo">
-            <Image
-              className="logo"
-              width={40}
-              height={40}
-              src="/Logo1.png"
-              alt="logo"
-            />
-          </div>
-          {'Saravanakumar'}
-        </Link>
-        <div className="settings">
-          <Settings toggleTheme={toggleTheme} currentTheme={currentTheme} />
-        </div>
-        <style jsx>{`
-          .settings {
-            padding-top: 0.2rem;
-            padding-bottom: 0.2rem;
-            padding-left: 0.5rem;
-            margin: 0 0.5rem;
-          }
-        `}</style>
-        <MobileIcon onClick={handleOpen}>
-          {open ? (
-            <X size={30} weight="bold" />
-          ) : (
-            <List size={30} weight="bold" />
-          )}
-        </MobileIcon>
-      </div>
-      <NavMenu onClick={handleOpen} open={open}>
-        <ul>
-          <li>
-            <Link href={'/'}>
-              <span>{currentLang === 'ta' ? 'முகப்பு' : 'Home'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/about'}>
-              <span>{currentLang === 'ta' ? 'பற்றி' : 'About'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/experience'}>
-              <span>{currentLang === 'ta' ? 'அனுபவம்' : 'Experience'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/blog'}>
-              <span>{currentLang === 'ta' ? 'பதிவு' : 'Blogs'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/projects'}>
-              <span>{currentLang === 'ta' ? 'திட்டங்கள்' : 'Projects'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/resume'}>
-              <span>{currentLang === 'ta' ? 'சுயவிவரம்' : 'Resume'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/contact'}>
-              <span>{currentLang === 'ta' ? 'தொடர்பு' : 'Contact'}</span>
-            </Link>
-          </li>
-        </ul>
-        <Icons>
-          <Link
-            href={'https://github.com/Saravanakumar2003'}
-            target="_blank"
-            aria-label={currentLang === 'ta' ? 'கிட்ஹப் இணைப்பு' : 'Link to Github'}>
-            <FiGithub />
-          </Link>
-          <Link
-            href={'https://www.linkedin.com/in/saravanaramaswamy2003/'}
-            target="_blank"
-            aria-label={currentLang === 'ta' ? 'லிங்க்டின் இணைப்பு' : 'Link to Linkedin'}>
-            <FiLinkedin />
-          </Link>
-          <Link
-            href={'https://www.instagram.com/saravanakumar.me?utm_source=qr'}
-            target="_blank"
-            aria-label={currentLang === 'ta' ? 'இன்ஸ்டாகிராம்' : 'Instagram'}>
-            <FiInstagram />
-          </Link>
-          <Link
-            href={'https://api.whatsapp.com/send?phone=918838416187'}
-            target="_blank"
-            aria-label={currentLang === 'ta' ? 'வாட்ஸ்அப் மூலம் தொடர்பு கொள்ள' : 'Link to contact via WhatsApp'}>
-            <FaWhatsapp />
-          </Link>
-        </Icons>
-      </NavMenu>
-    </HeaderContainer>
-  );
+  return (
+    <HeaderContainer style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1000 }}>
+      <div className="mobile-content" style={{ display: 'flex' }}>
+        <Link href={'/'}>
+          <div className="logo">
+            <Image
+              className="logo"
+              width={40}
+              height={40}
+              src="/Logo1.png"
+              alt="logo"
+            />
+          </div>
+          {'MD. MASUD RANA'}
+        </Link>
+        <div className="settings">
+          <Settings toggleTheme={toggleTheme} currentTheme={currentTheme} />
+        </div>
+        <style jsx>{`
+          .settings {
+            padding-top: 0.2rem;
+            padding-bottom: 0.2rem;
+            padding-left: 0.5rem;
+            margin: 0 0.5rem;
+          }
+        `}</style>
+        <MobileIcon onClick={handleOpen}>
+          {open ? (
+            <X size={30} weight="bold" />
+          ) : (
+            <List size={30} weight="bold" />
+          )}
+        </MobileIcon>
+      </div>
+      <NavMenu onClick={handleOpen} open={open}>
+        <ul>
+          <li>
+            <Link href={'/'}>
+              <span>{currentLang === 'bn' ? 'হোম' : 'Home'}</span>
+            </Link>
+          </li>
+          <li>
+            <Link href={'/about'}>
+              <span>{currentLang === 'bn' ? 'সম্পর্কিত' : 'About'}</span>
+            </Link>
+          </li>
+          <li>
+            <Link href={'/experience'}>
+              <span>{currentLang === 'bn' ? 'অভিজ্ঞতা' : 'Experience'}</span>
+            </Link>
+          </li>
+          <li>
+            <Link href={'/blog'}>
+              <span>{currentLang === 'bn' ? 'ব্লগ' : 'Blogs'}</span>
+            </Link>
+          </li>
+          <li>
+            <Link href={'/projects'}>
+              <span>{currentLang === 'bn' ? 'প্রজেক্ট' : 'Projects'}</span>
+            </Link>
+          </li>
+          <li>
+            <Link href={'/resume'}>
+              <span>{currentLang === 'bn' ? 'রেজুমে' : 'Resume'}</span>
+            </Link>
+          </li>
+          <li>
+            <Link href={'/contact'}>
+              <span>{currentLang === 'bn' ? 'যোগাযোগ' : 'Contact'}</span>
+            </Link>
+          </li>
+        </ul>
+        <Icons>
+          <Link
+            href={'https://github.com/masudrana234'} // Replaced with a more relevant GitHub
+            target="_blank"
+            aria-label={currentLang === 'bn' ? 'গিটহাব লিঙ্ক' : 'Link to Github'}>
+            <FiGithub />
+          </Link>
+          <Link
+            href={'https://www.linkedin.com/in/masudrana7'} // Updated LinkedIn
+            target="_blank"
+            aria-label={currentLang === 'bn' ? 'লিঙ্কডইন লিঙ্ক' : 'Link to Linkedin'}>
+            <FiLinkedin />
+          </Link>
+          <Link
+            href={'https://www.instagram.com/masudrana_rm'} // Replaced with a more relevant Instagram
+            target="_blank"
+            aria-label={currentLang === 'bn' ? 'ইনস্টাগ্রাম' : 'Instagram'}>
+            <FiInstagram />
+          </Link>
+          <Link
+            href={'https://wa.me/8801308202656'} // Replaced with a more relevant WhatsApp
+            target="_blank"
+            aria-label={currentLang === 'bn' ? 'হোয়াটসঅ্যাপের মাধ্যমে যোগাযোগ করতে' : 'Link to contact via WhatsApp'}>
+            <FaWhatsapp />
+          </Link>
+        </Icons>
+      </NavMenu>
+    </HeaderContainer>
+  );
 }
