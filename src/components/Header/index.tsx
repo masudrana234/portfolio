8bb33a47e36f99418bec99914774734e6e69fd3b
Masud Rana <masud202350@gmail.com>
2025-08-10 15:54:33 +0600

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,132 +1,305 @@
-import { useState, useEffect } from 'react'
-import Link from 'next/link'
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
 import Image from 'next/image'
-import { HeaderContainer, MobileIcon, NavMenu, Icons } from './styles'
-import { List, X } from 'phosphor-react'
-import { FiGithub, FiLinkedin, FiInstagram } from 'react-icons/fi'
-import { FaWhatsapp } from 'react-icons/fa'
-import { Button } from '../../styles/styles'
-import { useThemeContext } from '../../context/ThemeContext'
-import { FaSun, FaMoon } from 'react-icons/fa';
-import LanguageSwitcher from '../Language/index';
-import Settings from '../Settings/SettingsButton';
-import AudioPlayer from '../Music/AudioPlayer';
-import { useTranslation } from 'react-i18next'
-import { useRouter } from 'next/router'
+import Link from 'next/link'
+import Typewriter from 'typewriter-effect'
+import { Button, ButtonPrimary, Container } from '../../styles/styles'
+import { Content, ImgHome, HomeText, Card, Announcement, Logo, Box } from './styles'
+import { FiArrowRight } from 'react-icons/fi'
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { useTranslation } from 'react-i18next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+import * as htmlToImage from 'html-to-image';
+import Confetti from 'react-confetti';
 
-export function Header() {
-  const [open, setOpen] = useState(false);
-  const { currentTheme, toggleTheme } = useThemeContext(); 
-  const handleOpen = () => {
-    setOpen(!open);
-  };
-  const { t, i18n } = useTranslation('common');
-  const router = useRouter();
+const GOOGLE_FORM_ACTION_URL2 = process.env.NEXT_PUBLIC_GOOGLE_FORM_ACTION_URL2;
+const NAME_ENTRY_ID2 = process.env.NEXT_PUBLIC_NAME_ENTRY_ID2;
+
+Modal.setAppElement('#__next');
+
+export function HomeHero() {
+  const { t } = useTranslation('common');
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalIsOpen2, setModalIsOpen2] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [date, setDate] = useState('');
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [showCard, setShowCard] = useState(false);
+  const cardRef = useRef<HTMLDivElement>(null);
   const [currentLang, setCurrentLang] = useState<'en' | 'ta'>('en');
+  const router = useRouter();
+
   useEffect(() => {
     const { locale } = router;
     setCurrentLang(locale as 'en' | 'ta');
   }, [router.locale]);
 
+  function openModal2() {
+    setModalIsOpen2(true);
+  }
+
+  useEffect(() => {
+    // Fetch the current date using JavaScript's built-in Date object
+    const currentDate = new Date().toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    });
+    setDate(currentDate);
+  }, []);
+
+  function closeModal2() {
+    setModalIsOpen2(false);
+  }
+
+  async function handleGenerate() {
+    if (userName.trim() === '') {
+      alert('Please enter a name!');
+      return;
+    }
+
+    setShowCard(true);
+
+    try {
+      if (!GOOGLE_FORM_ACTION_URL2) {
+        console.error('Google Form action URL is not defined');
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append(NAME_ENTRY_ID2 || '', userName);
+
+      const response = await fetch(GOOGLE_FORM_ACTION_URL2, {
+        method: 'POST',
+        body: formData,
+        mode: 'no-cors',
+      });
+
+      if (response) {
+        setUserName('');
+      } else {
+        console.error('Failed to submit the form');
+      }
+    } catch (error) {
+      console.error('Error submitting the form:', error);
+    }
+
+    // Trigger confetti
+    setShowConfetti(true);
+
+    // Stop confetti after 3 seconds
+    setTimeout(() => {
+      setShowConfetti(false);
+    }, 10000);
+  }
+
+  async function downloadCard() {
+    if (cardRef.current) {
+      try {
+        const dataUrl = await htmlToImage.toPng(cardRef.current); // Convert the card to PNG
+        const link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = 'business-card.png'; // Set the download filename
+        link.click(); // Trigger the download
+      } catch (error) {
+        console.error('Failed to generate the business card image:', error);
+      }
+    }
+  }
+
   return (
-    <HeaderContainer style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1000 }}>
-      <div className="mobile-content" style={{ display: 'flex' }}>
-        <Link href={'/'}>
-          <div className="logo">
+    <Container>
+      <Content>
+        <HomeText>
+          <p>
+            <span>👋🏻</span> {currentLang === 'ta' ? 'வணக்கம், எனது பெயர்' : 'Hello, my name is'}
+          </p>
+          <h1>
+            {currentLang === 'ta' ? 'சரவணகுமார் ரா' : 'Saravanakumar R'}
+            <span className="animation">
+              <Image
+                width={200}
+                height={200}
+                src="/vectors/triangle.svg"
+                alt="triangle"
+                loading='lazy'
+              />
+            </span>
+          </h1>
+          <h2>
+            <Typewriter
+              options={{
+                strings: [currentLang === 'ta' ? 'டெவலப்பர் | பேச்சாளர் | தன்னார்வலர்' : 'Developer | Speaker | Volunteer'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+
+          <div className="button">
+            <Link legacyBehavior href="#projects">
+              <ButtonPrimary>
+                <b>{currentLang === 'ta' ? 'திட்டங்களைக் காண்க' : 'See Portfolio'}</b>
+                <FiArrowRight style={{ marginBottom: '-0.3rem' }} size={20} />
+              </ButtonPrimary>
+            </Link>
+
+            {/* <ButtonPrimary onClick={openModal2}>
+              <b>{currentLang === 'ta' ? 'அட்டை உருவாக்கவும்' : 'Generate your card'}</b>
+              <FiArrowRight style={{ marginBottom: '-0.3rem' }} size={20} />
+            </ButtonPrimary> */}
+
+            <Modal
+              isOpen={modalIsOpen2}
+              onRequestClose={closeModal2}
+              contentLabel="Card Modal"
+              style={{
+                overlay: {
+                  backgroundColor: 'rgba(0, 0, 0, 0.75)',
+                },
+                content: {
+                  top: '50%',
+                  left: '50%',
+                  right: 'auto',
+                  bottom: 'auto',
+                  marginRight: '-50%',
+                  transform: 'translate(-50%, -50%)',
+                  backgroundColor: '#fff',
+                  borderRadius: '10px',
+                  padding: '10px',
+                  width: '70%', // Adjust width for responsiveness
+                  maxWidth: '600px', // Limit the maximum width
+                  height: 'auto',
+                },
+              }}
+            >
+              <h2 style={{ textAlign: 'center', color: 'black' }}>
+                {currentLang === 'ta' ? 'அட்டை உருவாக்கவும்' : 'Generate Card'}
+              </h2>
+              <input
+                type="text"
+                placeholder={currentLang === 'ta' ? 'உங்கள் பெயரை உள்ளிடவும்' : 'Enter your name'}
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                style={{
+                  width: '100%',
+                  padding: '10px',
+                  margin: '10px 0',
+                  borderRadius: '5px',
+                  border: '1px solid #ccc',
+                }}
+              />
+              <Button
+                onClick={handleGenerate}
+                style={{
+                  marginBottom: '10px',
+                  backgroundColor: 'blue',
+                  padding: '10px 20px',
+                  borderRadius: '5px',
+                  color: 'white',
+                  margin: '0 auto',
+                }}
+              >
+                Generate
+              </Button>
+              <div
+                ref={cardRef}
+                style={{
+                  width: '100%',
+                  maxWidth: '600px',
+                  height: 'auto',
+                  aspectRatio: '3 / 2',
+                  margin: '20px auto',
+                  padding: '20px',
+                  border: '1px solid #000',
+                  borderRadius: '10px',
+                  textAlign: 'center',
+                  backgroundColor: '#f9f9f9',
+                }}
+              >
+                {showCard && (
+                  <Box>
+                    <Card>
+                      <header>
+                        <span>{date || 'Loading...'}</span>
+                        <Logo>
+                          <Image
+                            width={50}
+                            height={50}
+                            src="/Logo1.png"
+                            alt="logo"
+                            loading="lazy"
+                          />
+                        </Logo>
+                        <span>DEV CARD</span>
+                      </header>
+                      <Announcement>
+                        <h3>Yo! </h3>
+                        <h1>{userName || 'Error'}</h1>
+                        <h3 className="italic">thanks for stopping by my world</h3>
+                      </Announcement>
+                      <span>https://saravavana.vercel.app/</span>
+                    </Card>
+                  </Box>
+                )}
+              </div>
+              <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'row' }}>
+                <Button onClick={downloadCard} style={{
+                  marginBottom: '10px',
+                  backgroundColor: 'blue',
+                  padding: '10px 20px',
+                  borderRadius: '5px',
+                  margin: '0 auto',
+                  color: 'white',
+                }}>
+                  {currentLang === 'ta' ? 'பதிவிறக்கவும்' : 'Download'}
+                </Button>
+                <Button
+                  onClick={closeModal2}
+                  style={{
+                    marginTop: '10px',
+                    paddingTop: '10px',
+                    backgroundColor: 'red',
+                    padding: '10px 20px',
+                    borderRadius: '5px',
+                    margin: '0 auto',
+                    color: 'white',
+                  }}
+                >
+                  {currentLang === 'ta' ? 'மூடு' : 'Close'}
+                </Button>
+              </div>
+            </Modal>
+          </div>
+        </HomeText>
+
+        {showConfetti && (
+          <Confetti
+            width={window.innerWidth}
+            height={window.innerHeight}
+            numberOfPieces={250}
+            gravity={0.5}
+            wind={0.05}
+          />
+        )}
+        <ImgHome>
+          <img className="home-img" src="/home.png" alt={currentLang === 'ta' ? 'முகப்பு படம்' : 'Image Home'} />
+
+          <div className="code">
             <Image
-              className="logo"
-              width={40}
-              height={40}
-              src="/Logo1.png"
-              alt="logo"
+              width={150}
+              height={50}
+              src="/vectors/dino.svg"
+              alt="dino"
+              loading="lazy"
             />
           </div>
-          {'Saravanakumar'}
-        </Link>
-        <div className="settings">
-          <Settings toggleTheme={toggleTheme} currentTheme={currentTheme} />
-        </div>
-        <style jsx>{`
-          .settings {
-            padding-top: 0.2rem;
-            padding-bottom: 0.2rem;
-            padding-left: 0.5rem;
-            margin: 0 0.5rem;
-          }
-        `}</style>
-        <MobileIcon onClick={handleOpen}>
-          {open ? (
-            <X size={30} weight="bold" />
-          ) : (
-            <List size={30} weight="bold" />
-          )}
-        </MobileIcon>
-      </div>
-      <NavMenu onClick={handleOpen} open={open}>
-        <ul>
-          <li>
-            <Link href={'/'}>
-              <span>{currentLang === 'ta' ? 'முகப்பு' : 'Home'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/about'}>
-              <span>{currentLang === 'ta' ? 'பற்றி' : 'About'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/experience'}>
-              <span>{currentLang === 'ta' ? 'அனுபவம்' : 'Experience'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/blog'}>
-              <span>{currentLang === 'ta' ? 'பதிவு' : 'Blogs'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/projects'}>
-              <span>{currentLang === 'ta' ? 'திட்டங்கள்' : 'Projects'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/resume'}>
-              <span>{currentLang === 'ta' ? 'சுயவிவரம்' : 'Resume'}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={'/contact'}>
-              <span>{currentLang === 'ta' ? 'தொடர்பு' : 'Contact'}</span>
-            </Link>
-          </li>
-        </ul>
-        <Icons>
-          <Link
-            href={'https://github.com/Saravanakumar2003'}
-            target="_blank"
-            aria-label={currentLang === 'ta' ? 'கிட்ஹப் இணைப்பு' : 'Link to Github'}>
-            <FiGithub />
-          </Link>
-          <Link
-            href={'https://www.linkedin.com/in/saravanaramaswamy2003/'}
-            target="_blank"
-            aria-label={currentLang === 'ta' ? 'லிங்க்டின் இணைப்பு' : 'Link to Linkedin'}>
-            <FiLinkedin />
-          </Link>
-          <Link
-            href={'https://www.instagram.com/saravanakumar.me?utm_source=qr'}
-            target="_blank"
-            aria-label={currentLang === 'ta' ? 'இன்ஸ்டாகிராம்' : 'Instagram'}>
-            <FiInstagram />
-          </Link>
-          <Link
-            href={'https://api.whatsapp.com/send?phone=918838416187'}
-            target="_blank"
-            aria-label={currentLang === 'ta' ? 'வாட்ஸ்அப் மூலம் தொடர்பு கொள்ள' : 'Link to contact via WhatsApp'}>
-            <FaWhatsapp />
-          </Link>
-        </Icons>
-      </NavMenu>
-    </HeaderContainer>
-  );
+        </ImgHome>
+
+      </Content >
+    </Container >
+  )
 }
