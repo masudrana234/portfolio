bf72f151c1c36e3e1d283374124b8cd341040423
Masud Rana <masud202350@gmail.com>
2025-08-12 01:07:53 +0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+
 import Head from "next/head";
 import { About } from "../components/CardAbout";
 import { Header } from "../components/Header";
@@ -14,8 +15,10 @@ import { Education } from "../components/Education";
 import { Achievements } from "../components/Achievements";
 import { Testimonials } from "../components/Testimonials";
 import { Publications } from "../components/Publications";
+import { Wakatime } from "../components/Coding/Wakatime";
 import { Certificates } from "../components/Certificates/index";
 import { Work_Experience } from "../components/Work/index";
+import { Jokes } from "../components/Misc/Joke";
 import { useTranslation } from 'react-i18next'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
@@ -23,32 +26,25 @@ import React from 'react'
 import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
+
 const botkey = process.env.NEXT_PUBLIC_BOTKEY_URL;
 
 export default function Home() {
-  const { t, i18n } = useTranslation('common');
+  const { t, i18n } = useTranslation('common'); // Use the 'common' namespace
   const router = useRouter();
-  const [currentLang, setCurrentLang] = useState<'en' | 'bn'>('en');
+  const [currentLang, setCurrentLang] = useState<'en' | 'ta'>('en');
 
   useEffect(() => {
     const { locale } = router;
-    setCurrentLang(locale as 'en' | 'bn');
+    setCurrentLang(locale as 'en' | 'ta');
   }, [router.locale]);
+
 
   return (
     <>
       <Head>
-        <title>{currentLang === 'bn' ? 'মোঃ মাসুদ রানার পোর্টফোলিও' : 'Md. Masud Rana\'s Portfolio'}</title>
-        <meta 
-          name="description" 
-          content={currentLang === 'bn' 
-            ? 'ব্যবস্থাপনা স্নাতক এবং ডিজিটাল মার্কেটিং পেশাদার মোঃ মাসুদ রানার পেশাদার পোর্টফোলিও' 
-            : 'Professional portfolio of Md. Masud Rana, Management Graduate and Digital Marketing Professional'}
-        />
-        <meta property="og:title" content={currentLang === 'bn' ? 'মোঃ মাসুদ রানার পোর্টফোলিও' : 'Md. Masud Rana\'s Portfolio'} />
-        <meta property="og:url" content="https://masud7.vercel.app" />
+        <title>{currentLang === 'ta' ? 'சரவணகுமாரின் போர்ட்ஃபோலியோ' : ' Saravanakumar\'s Portfolio'} </title>
       </Head>
-      
       <ScrollTop />
       <Section>
         <HomeHero />
@@ -62,16 +58,22 @@ export default function Home() {
         <Publications />
         <Certificates />
         <Projects />
+        <Wakatime />
         <Testimonials />
+        <Jokes />
         <CardContact />
       </Section>
 
       <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
-      <script src={botkey} defer></script>
+      <script
+        src={botkey}
+        defer
+      ></script>
       <Footer />
     </>
   );
 }
+
 
 export const getStaticProps: GetStaticProps = async ({ locale = 'en' }) => ({
   props: {
