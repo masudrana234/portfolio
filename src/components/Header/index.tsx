6b5f3cac113477e4f7452eb5211cd51ee002ad67
Masud Rana <masud202350@gmail.com>
2025-08-12 18:57:30 +0600

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -41,7 +41,7 @@ export function Header() {
               alt="logo"
             />
           </div>
-          {'Masud Rana\'s Portfolio Site'}
+          {'Masud Rana\'s Portfolio'}
         </Link>
         <div className="settings">
           <Settings toggleTheme={toggleTheme} currentTheme={currentTheme} />
@@ -66,37 +66,37 @@ export function Header() {
         <ul>
           <li>
             <Link href={'/'}>
-              <span>{currentLang === 'ta' ? 'முகப்பு' : 'Home'}</span>
+              <span>{currentLang === 'ta' ? 'হোম' : 'Home'}</span>
             </Link>
           </li>
           <li>
             <Link href={'/about'}>
-              <span>{currentLang === 'ta' ? 'பற்றி' : 'About'}</span>
+              <span>{currentLang === 'ta' ? 'সম্পর্কে' : 'About'}</span>
             </Link>
           </li>
           <li>
             <Link href={'/experience'}>
-              <span>{currentLang === 'ta' ? 'அனுபவம்' : 'Experience'}</span>
+              <span>{currentLang === 'ta' ? 'অভিজ্ঞতা' : 'Experience'}</span>
             </Link>
           </li>
           <li>
             <Link href={'/blog'}>
-              <span>{currentLang === 'ta' ? 'பதிவு' : 'Blogs'}</span>
+              <span>{currentLang === 'ta' ? 'ব্লগ' : 'Blogs'}</span>
             </Link>
           </li>
           <li>
             <Link href={'/projects'}>
-              <span>{currentLang === 'ta' ? 'திட்டங்கள்' : 'Projects'}</span>
+              <span>{currentLang === 'ta' ? 'প্রকল্প' : 'Projects'}</span>
             </Link>
           </li>
           <li>
             <Link href={'/resume'}>
-              <span>{currentLang === 'ta' ? 'சுயவிவரம்' : 'Resume'}</span>
+              <span>{currentLang === 'ta' ? 'রিজিউম' : 'Resume'}</span>
             </Link>
           </li>
           <li>
             <Link href={'/contact'}>
-              <span>{currentLang === 'ta' ? 'தொடர்பு' : 'Contact'}</span>
+              <span>{currentLang === 'ta' ? 'যোগাযোগ' : 'Contact'}</span>
             </Link>
           </li>
         </ul>
@@ -104,25 +104,25 @@ export function Header() {
           <Link
             href={'https://github.com/masudrana234'}
             target="_blank"
-            aria-label={currentLang === 'ta' ? 'கிட்ஹப் இணைப்பு' : 'Link to Github'}>
+            aria-label={currentLang === 'ta' ? 'গিটহাব লিংক' : 'Link to Github'}>
             <FiGithub />
           </Link>
           <Link
             href={'https://www.linkedin.com/in/masudrana7/'}
             target="_blank"
-            aria-label={currentLang === 'ta' ? 'லிங்க்டின் இணைப்பு' : 'Link to Linkedin'}>
+            aria-label={currentLang === 'ta' ? 'লিংকড-ইন লিংক' : 'Link to Linkedin'}>
             <FiLinkedin />
           </Link>
           <Link
             href={'https://www.instagram.com/masudrana_rm?utm_source=qr'}
             target="_blank"
-            aria-label={currentLang === 'ta' ? 'இன்ஸ்டாகிராம்' : 'Instagram'}>
+            aria-label={currentLang === 'ta' ? 'ইনস্টাগ্রাম' : 'Instagram'}>
             <FiInstagram />
           </Link>
           <Link
             href={'https://api.whatsapp.com/send?phone=8801308202656'}
             target="_blank"
-            aria-label={currentLang === 'ta' ? 'வாட்ஸ்அப் மூலம் தொடர்பு கொள்ள' : 'Link to contact via WhatsApp'}>
+            aria-label={currentLang === 'ta' ? 'হোয়াটসঅ্যাপে যোগাযোগ করুন' : 'Link to contact via WhatsApp'}>
             <FaWhatsapp />
           </Link>
         </Icons>
@@ -131,6 +131,7 @@ export function Header() {
   );
 
 }
+
 
 
 
